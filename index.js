0960bdec7863d808ee0b6bfcd293da2d54f21609
Galuyk Viktor <zzzJaguar777zzz@yandex.ru>
2023-10-23 23:54:25 +0300

    added logger

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const usersRouter = require('./routes/users');
+const logger = require('morgan');
 
+const HOST = '0.0.0.0';
+const PORT = Number(process.env.PORT) || 3000;
 const app = express();
 
+app.use(logger('dev'));
 app.use('/users', usersRouter);
 
 app.listen({
-  host: '0.0.0.0',
-  port: Number(process.env.PORT) || 3000,
+  host: HOST,
+  port: PORT,
 }, (err, address) => {
   if (err) {
     console.error('server starting error', err);
     process.exit(1);
   }
 
-  console.warn('listening on %s.', address);
+  console.warn('listening on %s.', PORT);
 });
 
 module.exports = app;
