a3a428c42bf92a1f2bed5789a6173c5cbfc2b038
Galuyk Viktor <zzzJaguar777zzz@yandex.ru>
2023-10-23 23:56:22 +0300

    added dotenv

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const usersRouter = require('./routes/users');
 const logger = require('morgan');
+require('dotenv').config();
+// console.log(process.env)
 
-const HOST = '0.0.0.0';
-const PORT = Number(process.env.PORT) || 3000;
+const API_HOST = process.env.API_HOST;
+const API_PORT = Number(process.env.API_PORT) || 3000;
 const app = express();
 
 app.use(logger('dev'));
 app.use('/users', usersRouter);
 
 app.listen({
-  host: HOST,
-  port: PORT,
+  host: API_HOST,
+  port: API_PORT,
 }, (err, address) => {
   if (err) {
     console.error('server starting error', err);
     process.exit(1);
   }
 
-  console.warn('listening on %s.', PORT);
+  console.warn('listening on http://%s:%d.', API_HOST, API_PORT);
 });
 
 module.exports = app;
