332deab0973dce03f689dcb8b31cb379aa85d731
Galuyk Viktor <zzzJaguar777zzz@yandex.ru>
2023-10-25 23:35:46 +0300

    cleared code

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,9 @@ import { Router } from 'express';
 import usersRouter from './users.js';
 import healthCheckRouter from './healthCheck.js';
 
-// function applyRoutes(app) {
 const router = Router();
 
 router.use('/', healthCheckRouter);
 router.use('/users', usersRouter);
-// }
 
 export default router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
 const router = Router();
 
-// function users() {
-  /* GET users listing. */
+/* PUT users listing. */
 router.put('/', function(req, res, next) {
     res.send('respond with a resource');
   });
-// }
-
 
 export default router;
